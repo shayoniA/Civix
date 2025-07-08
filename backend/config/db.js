9c46484563488ceffc110589e81b597d1e4f040f
Shivu384 <shivamarora382004@gmail.com>
2025-07-08 10:50:49 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
-<<<<<<< fix/auth
-const pg = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new pg.Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
     ? { rejectUnauthorized: false }
@@ -10,14 +9,3 @@ const pool = new pg.Pool({
 });
 
 module.exports = pool;
-=======
-const {Pool} =require('pg');
-const pool= new Pool({
-    connectionString:process.env.DATABASE_URL,
-    ssl:{
-        rejectUnauthorized:false
-    },
-});
-
-module.exports=pool;
->>>>>>> main
--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,30 @@
-
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import Home from './Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import PrivateRoute from './components/PrivateRoute';
 import AdminDashboard from './components/AdminDashboard';
 import Error404 from './components/Error404';
+import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import ScrollToTop from './components/ScrollToTop';
-
-// Newly added pages
 import About from "./Pages/About";
 import Privacy from "./Pages/Privacy";
 import Terms from "./Pages/Terms";
 import Contact from "./Pages/Contact";
-import ReportIssue from "./Pages/ReportIssue"
+import ReportIssue from "./Pages/ReportIssue";
 import ServerError from "./components/ServerError";
 import DownloadAndroid from './Pages/DownloadAndroid';
 import DownloadIOS from './Pages/DownloadIOS';
+import NewIssue from './Pages/NewIssue';
+import IssueDetail from './Pages/IssueDetail';
 
 const App = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/report-issue" element={<ReportIssue />} />        
-        <Route path="*" element={<Error404 />} />
-        <Route path='/download-android' element={<DownloadAndroid/>}/>
-        <Route path='/download-ios' element={<DownloadIOS/>}/>
-
-        {/* Protected routes */}
-        <Route 
-          path="/admin/dashboard" 
-          element={
-            <PrivateRoute allowedRoles={['admin']}>
-              <AdminDashboard />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/home" 
-          element={
-            <PrivateRoute allowedRoles={['user', 'admin']}>
-              <Home />
-            </PrivateRoute>
-          } 
-        />
-
-      <Route path="/500" element={<ServerError />} />
-
-      </Routes>
-
-      {/* Footer */}
-     
-    </BrowserRouter>
-  );
-};
-
-export default App;
-// src/App.jsx
-import { Toaster } from 'react-hot-toast';
-import { Routes, Route } from 'react-router-dom';
-// Import your other components...
-
-function App() {
-  return (
-    <>
-      {/* Toast Provider (should be near root) */}
       <Toaster
         position="top-right"
         toastOptions={{
@@ -83,31 +32,64 @@ function App() {
           duration: 4000,
           success: {
             iconTheme: {
-              primary: '#10B981', // Emerald 500
+              primary: '#10B981',
               secondary: 'white',
             },
           },
           error: {
             iconTheme: {
-              primary: '#EF4444', // Red 500
+              primary: '#EF4444',
               secondary: 'white',
             },
           },
         }}
       />
-
-      {/* Your existing layout */}
+      
       <Navbar />
       
       <main className="container mx-auto p-4">
         <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/report-issue" element={<ReportIssue />} />
+          <Route path="/download-android" element={<DownloadAndroid />} />
+          <Route path="/download-ios" element={<DownloadIOS />} />
           <Route path="/issues/new" element={<NewIssue />} />
           <Route path="/issues/:id" element={<IssueDetail />} />
-          {/* Other routes... */}
+          <Route path="/500" element={<ServerError />} />
+
+          {/* Protected routes */}
+          <Route 
+            path="/admin/dashboard" 
+            element={
+              <PrivateRoute allowedRoles={['admin']}>
+                <AdminDashboard />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/home" 
+            element={
+              <PrivateRoute allowedRoles={['user', 'admin']}>
+                <Home />
+              </PrivateRoute>
+            } 
+          />
+
+          {/* 404 Catch-all */}
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </main>
 
       <Footer />
-    </>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;

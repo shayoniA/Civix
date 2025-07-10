6d117c61287602ecfae4c933383f8536d2365c53
Harsh Srivastava <harshme08@gmail.com>
2025-07-10 22:51:01 +0530

    bugfix

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="w-full text-center py-4 border-t mt-8 text-gray-500 bg-white dark:bg-gray-900">
+    Civix &copy; {new Date().getFullYear()} - All rights reserved.
+  </footer>
+);
+
+export default Footer;

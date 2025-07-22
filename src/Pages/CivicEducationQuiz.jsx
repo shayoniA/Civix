6865cc661ae8a9ca38d0ad9788df5bfd0b2b9b42
Harsh Srivastava <harshme08@gmail.com>
2025-07-22 16:58:13 +0530

    Revert "# 🚀 Pull Request: Frontend Refactor – State, Performance & Accessibility"

--- src/Pages/CivicEducationQuiz.jsx
+++
@@ -1,8 +0,0 @@
-// Add to quiz options:
-<div 
-  role="button"
-  tabIndex={0}
-  onKeyDown={(e) => e.key === 'Enter' && handleSelect(option)}
->
-  {option.text}
-</div>
--- src/stores/uiStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-
-export const useUIStore = create((set) => ({
-  darkMode: false,
-  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
-}));

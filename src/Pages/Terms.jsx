27f4593f455a33e79f4ebf80f1458643ead49d5b
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-07-23 18:54:56 +0530

    Removed terms.css and included inline css from tailwind to make the terms page look much better

--- src/Pages/Terms.jsx
+++ src/Pages/Terms.jsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import './Terms.css';
 
 function Terms() {
   return (
-    <div className="terms-container">
-      <h1 className="terms-title">Terms of Service</h1>
-      <p className="terms-text">
+    <div className="min-h-screen max-w-7xl mx-auto m-8 p-8 bg-gray-100 shadow-md rounded-lg dark:bg-[#111827]">
+      <h1 className="text-4xl  text-[#12b981] mb-8 border-b-2 pb-4">Terms of Service</h1>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         These Terms of Service govern your use of Civix. By accessing or using our platform, you agree to these terms.
       </p>
 
-      <h2 className="terms-subtitle">Use of Service</h2>
-      <p className="terms-text">
+      <h2 className="text-[#12b981] text-xl mb-6">Use of Service</h2>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         You agree to use Civix for lawful purposes only and not to misuse or interfere with the platform’s functionality.
       </p>
 
-      <h2 className="terms-subtitle">User Responsibilities</h2>
-      <p className="terms-text">
+      <h2 className="text-[#12b981] text-xl mb-6">User Responsibilities</h2>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         Users are responsible for the accuracy of the information they submit and must avoid submitting false or misleading reports.
       </p>
 
-      <h2 className="terms-subtitle">Platform Rights</h2>
-      <p className="terms-text">
+      <h2 className="text-[#12b981] text-xl mb-6">Platform Rights</h2>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         We reserve the right to modify, suspend, or discontinue the platform at any time without prior notice.
       </p>
 
-      <h2 className="terms-subtitle">Termination</h2>
-      <p className="terms-text">
+      <h2 className="text-[#12b981] text-xl mb-6">Termination</h2>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         We may suspend or terminate access if you violate these terms or engage in prohibited activities.
       </p>
 
-      <h2 className="terms-subtitle">Updates to Terms</h2>
-      <p className="terms-text">
+      <h2 className="text-[#12b981] text-xl mb-6">Updates to Terms</h2>
+      <p className="text-lg text-[#374151] mb-6 dark:text-gray-200">
         We may revise these terms periodically. Continued use of Civix constitutes your acceptance of any changes.
       </p>
 
-      <p className="terms-updated">Last updated: June 2025</p>
+      <p className="text-[#6b7280] text-sm flex justify-end dark:text-white">Last updated: June 2025</p>
     </div>
   );
 }
